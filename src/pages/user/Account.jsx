2776a54d63ca47536e07ed4b57e150406ca7031c
omkar mhaske <135412726+omkarmaske160@users.noreply.github.com>
2024-06-11 22:25:40 +0530

    adj

--- src/pages/user/Account.jsx
+++ src/pages/user/Account.jsx
@@ -176,20 +176,20 @@ const Stat = () => {
                 label: '# of Votes',
                 data: urlData && urlData.map(item => item.count),
                 backgroundColor: [
-                    'rgba(255, 255, 255, 0.2)', // white
-                    'rgba(255, 99, 132, 0.2)',  // red
-                    'rgba(75, 192, 192, 0.2)',  // green
-                    'rgba(54, 162, 235, 0.2)',  // blue
-                    'rgba(255, 206, 86, 0.2)',  // yellow
-                    'rgba(153, 102, 255, 0.2)', // purple
+                    'rgba(255, 255, 255, 0.8)', // bright white
+                    'rgba(255, 0, 0, 0.8)',     // bright red
+                    'rgba(0, 255, 0, 0.8)',     // bright green
+                    'rgba(0, 0, 255, 0.8)',     // bright blue
+                    'rgba(255, 255, 0, 0.8)',   // bright yellow
+                    'rgba(128, 0, 128, 0.8)',   // bright purple
                 ],
                 borderColor: [
-                    'rgba(255, 255, 255, 1)', // white
-                    'rgba(255, 99, 132, 1)',  // red
-                    'rgba(75, 192, 192, 1)',  // green
-                    'rgba(54, 162, 235, 1)',  // blue
-                    'rgba(255, 206, 86, 1)',  // yellow
-                    'rgba(153, 102, 255, 1)', // purple
+                    'rgba(255, 255, 255, 0.8)', // bright white
+                    'rgba(255, 0, 0, 0.8)',     // bright red
+                    'rgba(0, 255, 0, 0.8)',     // bright green
+                    'rgba(0, 0, 255, 0.8)',     // bright blue
+                    'rgba(255, 255, 0, 0.8)',   // bright yellow
+                    'rgba(128, 0, 128, 0.8)',   // bright purple
                 ],
                 borderWidth: 1,
             },
